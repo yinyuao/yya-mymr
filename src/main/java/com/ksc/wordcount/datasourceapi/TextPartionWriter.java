5b2bc1c632ecba0839c58b38aac56461e2da62ed
yinyuao <yinyuao@kingsoft.com>
2023-08-20 17:15:23 +0800

    fix: 修改schema

--- src/main/java/com/ksc/wordcount/datasourceapi/TextPartionWriter.java
+++ src/main/java/com/ksc/wordcount/datasourceapi/TextPartionWriter.java
@@ -1,11 +1,15 @@
 package com.ksc.wordcount.datasourceapi;
 
+import com.ksc.wordcount.driver.DriverEnv;
 import com.ksc.wordcount.task.KeyValue;
+import com.ksc.wordcount.worker.Executor;
+import com.ksc.wordcount.worker.ExecutorEnv;
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.reflect.ReflectData;
 
 import java.io.*;
 import java.util.stream.Stream;
@@ -35,8 +39,8 @@ public class TextPartionWriter implements PartionWriter<KeyValue>, Serializable 
     //todo 学生实现 将reducetask的计算结果写入结果文件中
     @Override
     public String write(Stream<KeyValue> stream) throws IOException {
-        String path = destDest + File.separator + "result.avro";
-        Schema schema = new Schema.Parser().parse(new File("src/main/schema/KeyValue.avsc"));
+        String path = destDest  + "/result.avro";
+        Schema schema = Schema.parse("{\"type\":\"record\",\"name\":\"KeyValue\",\"fields\":[{\"name\":\"key\",\"type\":\"string\"},{\"name\":\"value\",\"type\":\"int\"}]}");
         File file = new File(path);
         try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(new GenericDatumWriter<>(schema))) {
             dataFileWriter.create(schema, file);
